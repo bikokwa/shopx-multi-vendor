74c71a31db67a3d9ed61cf51f414963601f3f78d
bikokwa <34458781+bikokwa@users.noreply.github.com>
2026-07-29 06:37:29 +0300

    authentication system

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,12 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
