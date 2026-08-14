09b3d0d012e99e4ba9b076bfd640ea22134923e3
bikokwa <peterbikokwa@gmail.com>
2026-08-14 14:54:37 +0300

    account details

--- resources/views/frontend/dashboard/account/index.blade.php
+++ resources/views/frontend/dashboard/account/index.blade.php
@@ -1,33 +1,48 @@
-<div class="tab-pane fade" id="account-detail" role="tabpanel"
+@extends('frontend.dashboard.dashboard-app')
+
+@section('dashboard_contents')
+<div id="account-detail" role="tabpanel"
                                         aria-labelledby="account-detail-tab">
                                         <div class="card">
                                             <div class="card-header p-0">
                                                 <h5>Account Details</h5>
                                             </div>
                                             <div class="card-body p-0">
-                                                <p>Already have an account? <a href="page-login.html">Log in
-                                                        instead!</a></p>
+                                                <p>You can edit your account details here.</p>
                                                 <form method="post" name="enq">
                                                     <div class="row mt-30">
-                                                        <div class="form-group col-md-6">
-                                                            <label>First Name <span class="required">*</span></label>
+                                                        <div class="form-group col-md-12">
+                                                            <label>Name <span class="required">*</span></label>
                                                             <input required="" class="form-control" name="name"
                                                                 type="text" />
                                                         </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label>Last Name <span class="required">*</span></label>
-                                                            <input required="" class="form-control" name="phone" />
-                                                        </div>
-                                                        <div class="form-group col-md-12">
-                                                            <label>Display Name <span class="required">*</span></label>
-                                                            <input required="" class="form-control" name="dname"
-                                                                type="text" />
-                                                        </div>
+
+
                                                         <div class="form-group col-md-12">
                                                             <label>Email Address <span class="required">*</span></label>
                                                             <input required="" class="form-control" name="email"
                                                                 type="email" />
                                                         </div>
+
+                                                        <div class="col-md-12">
+                                                            <button type="submit"
+                                                                class="btn btn-fill-out submit font-weight-bold"
+                                                                name="submit" value="Submit">Save Change</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+
+                                        <div class="card mt-4">
+                                            <div class="card-header p-0">
+                                                <h5>Change Password</h5>
+                                            </div>
+                                            <div class="card-body p-0">
+                                                <p>You can change your password here.</p>
+                                                <form method="post" name="enq">
+                                                    <div class="row mt-30">
+                                                        
                                                         <div class="form-group col-md-12">
                                                             <label>Current Password <span
                                                                     class="required">*</span></label>
@@ -45,23 +60,7 @@
                                                             <input required="" class="form-control" name="cpassword"
                                                                 type="password" />
                                                         </div>
-                                                        <div class="form-group col-md-12">
-                                                            <label>Select Box <span class="required">*</span></label>
-                                                            <select class="select-active">
-                                                                <option value="#">656566</option>
-                                                                <option value="#">656566</option>
-                                                                <option value="#">656566</option>
-                                                                <option value="#">656566</option>
-                                                                <option value="#">656566</option>
-                                                                <option value="#">656566</option>
-                                                                <option value="#">656566</option>
-                                                            </select>
-                                                        </div>
-                                                        <div class="form-group col-md-12">
-                                                            <label>Message <span class="required">*</span></label>
-                                                            <textarea rows="6" class="form-control"
-                                                                placeholder="Message"></textarea>
-                                                        </div>
+
                                                         <div class="col-md-12">
                                                             <button type="submit"
                                                                 class="btn btn-fill-out submit font-weight-bold"
@@ -72,3 +71,5 @@
                                             </div>
                                         </div>
                                     </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Frontend\UserDashboardController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,16 +10,12 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+
+    /** Profile Routes */
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 });
 
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard.index');
-})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
