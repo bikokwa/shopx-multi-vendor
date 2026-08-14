02fc1eebdb68c196e4e65b78ffbe5f3d9466f4c4
bikokwa <peterbikokwa@gmail.com>
2026-08-14 08:51:51 +0300

    frontend - dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,9 @@ Route::get('/', function () {
     return view('frontend.home.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard.index');
