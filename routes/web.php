fd7001aa6a025a354b835a9aa06266be97893155
bikokwa <peterbikokwa@gmail.com>
2026-08-25 11:29:11 +0300

    working with vendor panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\Frontend\ProfileController;
+use App\Http\Controllers\Frontend\VendorDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,7 +18,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::put('/profile/password', [ProfileController::class, 'passwordUpdate'])->name('password.update');
 });
 
-
-
+/** Vendor Routes */
+Route::group(['prefix' => 'vendor', 'as' => 'vendor.', 'middleware' => ['auth', 'verified', 'vendor']], function () {
+    Route::get('/dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
