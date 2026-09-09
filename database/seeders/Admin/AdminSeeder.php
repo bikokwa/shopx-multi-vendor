26e45f90a8ad4a72a2917fc39febdeda3ab21409
bikokwa <peterbikokwa@gmail.com>
2026-09-09 20:59:48 +0300

    creating super admin role seeder

--- database/seeders/Admin/AdminSeeder.php
+++ database/seeders/Admin/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders\Admin;
 use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
 {
@@ -13,10 +14,17 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create a super admin user
         $admin = new Admin();
         $admin->name = 'SUPER ADMIN';
         $admin->email = 'admin@gmail.com';
         $admin->password = bcrypt('1234');
         $admin->save();
+
+        // Create the "Super Admin" role
+        Role::create(['name' => 'Super Admin', 'guard_name' => 'admin']);
+
+        // Assign the "Super Admin" role to the user
+        $admin->assignRole('Super Admin');
     }
 }
