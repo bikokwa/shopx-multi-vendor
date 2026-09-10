b0edc46b8758a8ce912c78a47274be6c8843a6d4
bikokwa <peterbikokwa@gmail.com>
2026-09-10 18:31:09 +0300

    fixing profile icon, name, role name

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1065,10 +1065,10 @@
             </div>
             <div class="nav-item dropdown">
               <a href="#" class="nav-link d-flex lh-1 p-0 px-2" data-bs-toggle="dropdown" aria-label="Open user menu">
-                <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"> </span>
+                <span class="avatar avatar-sm" style="background-image: url({{ asset(auth('admin')->user()->avatar) }})"> </span>
                 <div class="d-none d-xl-block ps-2">
-                  <div>Paweł Kuna</div>
-                  <div class="mt-1 small text-secondary">UI Designer</div>
+                  <div>{{ auth('admin')->user()->name }}</div>
+                  <div class="mt-1 small text-secondary">{{ auth('admin')->user()->getRoleNames()?->first() }}</div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
